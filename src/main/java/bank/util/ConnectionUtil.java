5982ad0a7bd9bcd444ab27f8ec0ed9ad2db26605
Megala J <megalajeeva1999@gmail.com>
2020-02-07 10:07:46 +0530

    Updated jar file

--- src/main/java/bank/util/ConnectionUtil.java
+++ src/main/java/bank/util/ConnectionUtil.java
@@ -2,14 +2,20 @@ package bank.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import logger.Logger;
 
 public class ConnectionUtil {
+	private static final  Logger LOGGER = Logger.getInstance();
 	private ConnectionUtil() {
 		throw new IllegalStateException("Utility class");
 	}
-public static Connection getconnection() throws SQLException{
+public static Connection getconnection() throws Exception{
 		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			LOGGER.error(e);
+		}
 		String server="CSLH2026";
 		String userName="system";
 		String password="oracle";
